c7e20234f092d63ac0e36f0797d5362f4ce7fc88
Ivan Mikhailov <imihajlow@gmail.com>
2016-12-18 23:00:38 +0100

    Simple chat bot
    Recording of conversations;
    Converting recorded conversation into bot db;
    Using Fuse Js to fool people.

--- main.js
+++ main.js
@@ -5,18 +5,26 @@
 // @description  Fool people in chat
 // @author       Ivan Mikhailov
 // @match        https://chatvdvoem.ru/
+// @require      http://fusejs.io/assets/js/fuse.min.js
 // @grant        none
 // ==/UserScript==
 
+var recordingEnabled = false;
+var reconnectEnabled = true;
+var botEnabled = true;
+
+
 var sendingMessage = false;
+var messageTimer = null;
 
 function sendMsg(msg) {
     sendingMessage = true;
     var timeout = msg.length * 50 + 500;
     $("#text").val(msg);
-    setTimeout(function() {
+    messageTimer = setTimeout(function() {
         $("#text_send").click();
         sendingMessage = false;
+        messageTimer = null;
     }, timeout);
 }
 
@@ -29,17 +37,24 @@ function disconnect() {
 }
 
 function restart() {
+    if (messageTimer) {
+        clearTimeout(messageTimer);
+        messageTimer = null;
+    }
     disconnect();
-    setTimeout(startChat, 200);
+    //setTimeout(startChat, 200);
 }
 
 function isConnected() {
     return !$("#text_send").prop("disabled");
 }
 
+var disconnectCallbackFired = false;
+
 function waitConnection(callback) {
     function waiter() {
         if (isConnected()) {
+            disconnectCallbackFired = false;
             waitSilence();
             callback();
         } else {
@@ -73,35 +88,111 @@ function init(onMessage, onDisconnect) {
         var last = $("#messages>ol").children().last();
         if (last.hasClass("disconnected")) {
             console.log("disconnected");
-            onDisconnect();
+            if (!disconnectCallbackFired) {
+                onDisconnect();
+            }
+            disconnectCallbackFired = true;
         } else if (last.hasClass("messageFrom")) {
             var msg = last.find(".message").text();
             console.log("got message:", msg);
             waitSilence();
             onMessage(msg);
+        } else if (last.hasClass("messageTo")) {
+            var msg = last.find(".message").text();
+            console.log("sent message:", msg);
+            onOutgoingMessage(msg);
         }
     });
     waitConnection(onConnect);
 }
 
-var botState = "start";
+var conversation = [];
 
 function onConnect() {
+    conversation = [];
 }
 
 function onDisconnect() {
-    startChat();
-    waitConnection(onConnect);
+    if (recordingEnabled) {
+        if (conversation.length > 10) { // Save only meaningful conversations
+            var chatsJson = localStorage.getItem("chats");
+            if (chatsJson === null) {
+                chatsJson = '[]';
+            }
+            var chats = JSON.parse(chatsJson);
+            chats[chats.length] = conversation;
+            try {
+                localStorage.setItem("chats", JSON.stringify(chats));
+            } catch (e) {
+                alert("Unable to save in localStorage");
+            }
+        }
+    }
+    if (reconnectEnabled) {
+        startChat();
+        waitConnection(onConnect);
+    }
 }
 
 function onMessage(msg) {
+    if (recordingEnabled) {
+        conversation[conversation.length] = { "t": "i", "m": msg };
+    }
+    if (botEnabled) {
+        reply = getReply(msg);
+        if (reply !== null) {
+            sendMsg(reply);
+        }
+    }
+}
+
+function onOutgoingMessage(msg) {
+    if (recordingEnabled) {
+        conversation[conversation.length] = { "t": "o", "m": msg };
+    }
 }
 
 function onSilence() {
-    sendMsg("че молчишь, мудила?");
+    if (recordingEnabled) {
+        conversation[conversation.length] = { "t": "s" };
+    }
+}
+
+var fuse;
+
+function initBot() {
+    var options = {
+      shouldSort: true,
+      tokenize: true,
+      findAllMatches: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: ["i"]
+    };
+    fuse = new Fuse(db, options);
+}
+
+function getReply(input) {
+    if (!fuse) {
+        initBot();
+    }
+    result = fuse.search(input);
+    console.log("getReply", input, result);
+    if (result.length > 0) {
+        var items = result[0].o;
+        if (items.length > 0) {
+            return items[Math.floor(Math.random()*items.length)];
+        }
+    }
+    return null;
 }
 
 (function() {
     'use strict';
     init(onMessage, onDisconnect);
 })();
+
+var db = [];
